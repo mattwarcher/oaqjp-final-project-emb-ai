93c42feeefd26b4418b37980500da3654a3b659b
Matt Archer <mattwarcher87@gmail.com>
2026-07-24 00:49:27 -0400

    finished task 7

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -2,7 +2,7 @@ import requests
 import json
 
 def emotion_detector(text_to_analyze):
-    
+
     url = (
         "https://sn-watson-emotion.labs.skills.network/"
         "v1/watson.runtime.nlp.v1/NlpService/EmotionPredict"
@@ -21,23 +21,28 @@ def emotion_detector(text_to_analyze):
 
     response = requests.post(url, json=input_json, headers=headers)
 
+    # Handle blank input
+    if response.status_code == 400:
+        return {
+            "anger": None,
+            "disgust": None,
+            "fear": None,
+            "joy": None,
+            "sadness": None,
+            "dominant_emotion": None
+        }
+
     formatted_response = json.loads(response.text)
 
     emotions = formatted_response["emotionPredictions"][0]["emotion"]
 
-    anger = emotions["anger"]
-    disgust = emotions["disgust"]
-    fear = emotions["fear"]
-    joy = emotions["joy"]
-    sadness = emotions["sadness"]
-
     dominant_emotion = max(emotions, key=emotions.get)
 
     return {
-        "anger": anger,
-        "disgust": disgust,
-        "fear": fear,
-        "joy": joy,
-        "sadness": sadness,
+        "anger": emotions["anger"],
+        "disgust": emotions["disgust"],
+        "fear": emotions["fear"],
+        "joy": emotions["joy"],
+        "sadness": emotions["sadness"],
         "dominant_emotion": dominant_emotion
     }
--- server.py
+++ server.py
@@ -10,11 +10,15 @@ def render_index_page():
 
 @app.route("/emotionDetector")
 def emotion_detection():
+
     text_to_analyze = request.args.get("textToAnalyze")
 
     response = emotion_detector(text_to_analyze)
 
-    formatted_response = (
+    if response["dominant_emotion"] is None:
+        return "Invalid text! Please try again!"
+
+    return (
         f"For the given statement, the system response is "
         f"'anger': {response['anger']}, "
         f"'disgust': {response['disgust']}, "
